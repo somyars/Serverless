7735a7f9e9b7363d300b515adaddd34173b5215e
somyarsharma <43866866+somyarsharma@users.noreply.github.com>
2019-03-09 15:40:38 -0600

    Initial commit

---
+++ Code/lambda.py
@@ -0,0 +1,31 @@
+import json
+def json2xml(json_obj, line_padding=""):
+    result_list = list()
+
+    json_obj_type = type(json_obj)
+
+    if json_obj_type is list:
+        for sub_elem in json_obj:
+            result_list.append(json2xml(sub_elem, line_padding))
+
+        return "\n".join(result_list)
+
+    if json_obj_type is dict:
+        for tag_name in json_obj:
+            sub_obj = json_obj[tag_name]
+            result_list.append("%s<%s>" % (line_padding, tag_name))
+            result_list.append(json2xml(sub_obj, "\t" + line_padding))
+            result_list.append("%s</%s>" % (line_padding, tag_name))
+
+        return "\n".join(result_list)
+
+    return "%s%s" % (line_padding, json_obj)
+    
+def lambda_handler(event, context):
+    temp = json.dumps(event['body'])
+    j = json.loads(temp)
+    output = json2xml(j)
+    return {
+        'statusCode': 200,
+        'body': output
+    }
